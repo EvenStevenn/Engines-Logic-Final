8b5937b7870a03553667b1592a2db57cbf99486f
Steven Mirasola <scmirasola@gmail.com>
2019-12-05 19:16:07 -0500

    Added enemy types

---
+++ Engines-Logic-Final/Assets/Scripts/EnemySO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Enemy", menuName ="Create Enemy SO", order = 1)]
+public class EnemySO : ScriptableObject
+{
+    public int health;
+    public int numOfCoins;
+    public bool isDead;
+    public int EnemySpeed;
+
+}
