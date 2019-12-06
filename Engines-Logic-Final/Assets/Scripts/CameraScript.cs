2e2d24394f27d06059862ce9d9fbea6974502027
QA3R <46014750+QA3R@users.noreply.github.com>
2019-12-06 14:53:27 -0500

    Added a butt-ton of things
    PlayerScripts, CameraScript, and folders for organization added.

---
+++ Engines-Logic-Final/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private float yaw;
+    private float pitch;
+    public float cameraDistance = 2.0f;
+    public float mouseSensitivity = 10.0f;
+
+    public Vector2 pitchMinMax = new Vector2 (-40,85);
+
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        
+        Vector3 playerRotation = new Vector3(pitch, yaw);
+        transform.eulerAngles =playerRotation;
+
+        transform.position = player.position - transform.forward * cameraDistance;
+        
+    }
+}
---
+++ Engines-Logic-Final/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{ 
+    public float playerSpeed = 6.0f;
+    public float playerSpeedCarrying = 2.0f;
+
+    public float turnSmoothTime = .12f;
+    float turnSmoothVelocity;
+
+    public Transform cameraTransform;
+
+    
+    void Start()
+    {
+        Cursor.visible = false;
+        cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDirection = input.normalized;
+
+        if (inputDirection != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+        }
+        //bool carrying;
+
+        //float speed = ((carrying) ? playerSpeedCarrying : playerSpeed)* inputDirection.magnitude;
+
+        transform.Translate(transform.forward * playerSpeed * inputDirection.magnitude * Time.deltaTime, Space.World);
+
+        
+    }
+}
