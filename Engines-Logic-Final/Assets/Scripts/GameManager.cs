3c70348bb83784b44fa38cad2af9f70e1ce4c6fd
SkyeMcBride <smcbrid4@my.centennialcollege.ca>
2019-12-08 02:31:41 -0500

    MainMenu/PauseMenu

---
+++ Engines-Logic-Final/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int nextScene;
+    public Button startButton;
+    public Button exitButton;
+    public Button restartButton;
+    public Button mainmenuButton;
+    public GameObject pauseMenu;
+    public GameObject inGameHUD;
+    public GameObject mainCam;
+    public string mainScene;
+    public string mainmenuScene;
+    public bool paused;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu.activeSelf)
+        {
+            UnPause();
+        }
+        
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(mainScene);
+        Debug.Log("Loading game level...");
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainmenuScene);
+        Debug.Log("Loading main menu...");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting game...");
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Debug.Log("Game is paused.");
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Debug.Log("Game is resumed.");
+    }
+}
---
+++ Engines-Logic-Final/Assets/Scripts/PauseScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Debug.Log("Game is resumed.");
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Debug.Log("Game is paused.");
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
