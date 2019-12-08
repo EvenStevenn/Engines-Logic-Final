19023b4ab84a3fa00ed0f97743ea3349664a2814
QA3R <46014750+QA3R@users.noreply.github.com>
2019-12-08 06:18:13 -0500

    Player movement changes and Turret pickup functionality
    Converted player movement script from transform.translate to character controller (note that all tiles will now need the "Ground" layer for the player to fall properly. This change should be on all tile prefabs)
    
    Turret placement on nodes based on tag "TowerTile".

--- Engines-Logic-Final/Assets/Scripts/PlayerScript.cs
+++ Engines-Logic-Final/Assets/Scripts/PlayerScript.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 { 
+    public CharacterController controller;
+
     public float playerSpeed = 6.0f;
     public float playerSpeedCarrying = 2.0f;
-
     public float turnSmoothTime = .12f;
+    public float groundDistance = 0.4f;
+    public float gravity = -9.8f;
     float turnSmoothVelocity;
 
     public Transform cameraTransform;
     public Transform turretHolder;
+    public Transform groundCheck;
 
     public GameObject turret;
     public Rigidbody turretRB;
 
     public bool hasTurret;
+    bool isGrounded;
+
+    public LayerMask groundMask;
+
+    public Vector3 velocity;
 
     
     void Start()
@@ -37,16 +46,30 @@ public class PlayerScript : MonoBehaviour
 
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
         }
-        //bool carrying;
 
+        // will always move the player forward in both directions given the rotation of the player itself.
+        Vector3 move = (this.gameObject.transform.forward * Mathf.Abs(input.x) * playerSpeed) + (this.gameObject.transform.forward * Mathf.Abs(input.y) * playerSpeed);
+        controller.Move(move * Time.deltaTime);
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y< 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+
+        //code to change speed based on whether the player is carrying a turret
         //float speed = ((carrying) ? playerSpeedCarrying : playerSpeed)* inputDirection.magnitude;
 
-        transform.Translate(transform.forward * playerSpeed * inputDirection.magnitude * Time.deltaTime, Space.World);
+        //old code:
+        //transform.Translate(transform.forward * playerSpeed * inputDirection.magnitude * Time.deltaTime, Space.World);
+
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKey(KeyCode.E) && other.gameObject.tag == "Turret")
         {
             hasTurret = true;
             turretRB.isKinematic = true;
