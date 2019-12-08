9383f5e5c9179c0d6fd586d83240d8b2e187496e
Steven Mirasola <scmirasola@gmail.com>
2019-12-08 17:21:36 -0500

    small changes

---
+++ Engines-Logic-Final/Assets/Scripts/WaveSO.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Wave", menuName = "Create Wave SO", order = 2)]
+public class WaveSO : ScriptableObject
+{
+    List<GameObject> ListOfEnemiesinWave;
+}
