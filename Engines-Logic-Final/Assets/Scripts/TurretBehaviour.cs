2b167ae2e597a4ad72c7a464a16788001865b4f2
Lyssa <lyssa.tino@gmail.com>
2019-12-11 23:30:01 -0500

    Updated TurretBehaviour

--- Engines-Logic-Final/Assets/Scripts/TurretBehaviour.cs
+++ Engines-Logic-Final/Assets/Scripts/TurretBehaviour.cs
@@ -4,85 +4,28 @@ using UnityEngine;
 
 public class TurretBehaviour : MonoBehaviour
 {
-    public EnemyBehaviors eb;
     public GameObject forRotation;
     public Transform projectileSpawnPoint;
-
-    public void Start()
-    {
-        GameObject projectile = eb.projectilePrefab;
-    }
+    public GameObject projectilePrefab;
 
     public void OnTriggerEnter(Collider myCollider)
     {
         //check if what entered is an enemy
         if (myCollider.tag == "Enemy")
         {
-            DoAttack();
+            DoAttack(myCollider);
         }
     }
 
-    public void DoAttack()
+    public void DoAttack(Collider enemyCollider)
     {
+        //spawn a projectile
+        GameObject myPrefab = Instantiate(projectilePrefab, projectileSpawnPoint.position, Quaternion.identity);
 
+        //fire at enemy
+        Rigidbody rb = myPrefab.GetComponent<Rigidbody>();
+        myPrefab.transform.position = Vector3.MoveTowards(myPrefab.transform.position, enemyCollider.transform.position, 3);
     }
 
 
-    //private void OnTriggerStay(Collider other)
-    //{
-    //    //initiate attack sequence if enemy is detected
-    //    if (other.tag == "Enemy")
-    //    {
-    //        TurretAttack();
-    //    }
-
-    //    //move the projectile to enemyPos
-    //    if (eb.projectilePrefab != null)
-    //    {
-    //        eb.projectilePos = Vector3.MoveTowards(eb.turret.transform.position, eb.enemy.transform.position, 3);
-    //    }
-
-    //    ProjectileAttack();
-    //}
-
-    //void ProjectileAttack()
-    //{
-
-    //    float dist = Vector3.Distance(eb.projectilePos, eb.enemyPos);
-
-    //    if (dist < 5)
-    //    {
-    //        Debug.Log("Projectile collided with enemy...");
-
-    //        //damage
-    //        if (eb.enemyHP > 0)
-    //        {
-    //            eb.enemyHP -= 10;
-    //            Debug.Log("Enemy damaged... " + eb.enemyHP);
-    //        }
-
-    //        //death
-    //        else
-    //        {
-    //            Object.Destroy(eb.enemy);
-    //            Debug.Log("Enemy dead " + eb.enemyHP);
-    //            eb.coinCount.text += 20;
-    //            eb.enemySO.isDead = true;
-    //        }
-
-    //        Object.Destroy(eb.projectilePrefab);
-    //    }
-    //}
-
-    //void TurretAttack()
-    //{
-    //    eb.enemyPos = eb.enemy.transform.position;
-    //    eb.turretPos = eb.turret.transform.position;
-
-    //    Instantiate(eb.projectilePrefab, eb.turretPos, Quaternion.identity);
-    //    eb.projectileRB = GetComponent<Rigidbody>();
-    //    eb.projectileRB.AddForce(new Vector3(Random.Range(eb.projectileSpeedLow, eb.projectileSpeedHigh), Random.Range(eb.projectileSpeedLow, eb.projectileSpeedHigh)), ForceMode.Impulse);
-    //    eb.projectilePrefab.transform.position = eb.enemyPos;
-    //    Debug.Log("Turret attacking...");
-    //}
 }
