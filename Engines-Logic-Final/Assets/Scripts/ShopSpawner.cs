b0874cc3d7610656e00b13214a54f63b69c20128
Lyssa <lyssa.tino@gmail.com>
2019-12-12 12:53:55 -0500

    Added ShopSpawner

---
+++ Engines-Logic-Final/Assets/Scripts/ShopSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopSpawner : MonoBehaviour
+{
+    public GameObject turret;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                GameObject myTurret = Instantiate<GameObject>(turret, other.transform);
+                float myY = myTurret.transform.position.y;
+                myY += 0.5f;
+                myTurret.transform.Translate(other.transform.position.x, myY, other.transform.position.z);
+            }
+        }
+    }
+}
