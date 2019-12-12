f80aaf28bc1511f88d078a95af813b908652a71c
QA3R <46014750+QA3R@users.noreply.github.com>
2019-12-12 15:27:24 -0500

    Small Changes to turretbehaviour script

--- Engines-Logic-Final/Assets/Scripts/TurretBehaviour.cs
+++ Engines-Logic-Final/Assets/Scripts/TurretBehaviour.cs
@@ -4,44 +4,44 @@ using UnityEngine;
 
 public class TurretBehaviour : MonoBehaviour
 {
-
-    public GameObject projectilePrefab;
     public Transform projectileSpawnPoint;
     public GameObject bulletPrefab;
     public Rigidbody bulletRB;
     public GameObject enemy;
     public Vector3 enemyPos;
-    public bool canShoot;
+
+
 
     private void Start()
     {
-        bulletPrefab = GameObject.FindGameObjectWithTag("Bullet");
-        bulletRB = bulletPrefab.GetComponent<Rigidbody>();
-        canShoot = true;
+        //bulletPrefab = GameObject.FindGameObjectWithTag("Bullet");
+
     }
 
     private void Update()
     {
-        Vector3.MoveTowards(bulletPrefab.transform.position, enemyPos, 3*Time.deltaTime);
+
+    Vector3.MoveTowards(bulletPrefab.transform.position, enemyPos, 3*Time.deltaTime);
     }
 
     private void OnTriggerStay(Collider myCollider)
     {
         //turn turret to an enemy in range
-        if (myCollider.tag == "Enemy" && canShoot)
+        if (myCollider.tag == "Enemy")
         {
             gameObject.transform.rotation = Quaternion.LookRotation(myCollider.transform.position - this.gameObject.transform.position);
             StartCoroutine(DoAttack((myCollider)));
             enemy.transform.position = myCollider.transform.position;
             enemyPos = myCollider.transform.position;
+            bulletRB = bulletPrefab.GetComponent<Rigidbody>();
         }
     }
     
     public IEnumerator DoAttack (Collider enemyCollider)
     {
-        bulletPrefab = Instantiate(projectilePrefab, projectileSpawnPoint.position, Quaternion.identity);
-        canShoot = false;
+        bulletPrefab = Instantiate(bulletPrefab, projectileSpawnPoint.position, Quaternion.identity);
+
         yield return new WaitForSeconds(1.0f);
-        canShoot = true;
+        
     }
 }
