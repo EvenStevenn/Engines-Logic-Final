54545e98d6df2dd5ba8b08c18fb0171280cc05f9
Lyssa <lyssa.tino@gmail.com>
2019-12-12 12:24:35 -0500

    Fixed turret script

--- Engines-Logic-Final/Assets/Scripts/TurretBehaviour.cs
+++ Engines-Logic-Final/Assets/Scripts/TurretBehaviour.cs
@@ -6,63 +6,12 @@ public class TurretBehaviour : MonoBehaviour
 {
     public EnemyBehaviors eb;
 
-    private void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider myCollider)
     {
-        //initiate attack sequence if enemy is detected
-        if (other.gameObject.tag == "Enemy")
+        //turn turret to an enemy in range
+        if(myCollider.tag == "Enemy")
         {
-            Debug.Log("enemy in range");
-            TurretAttack();
+            gameObject.transform.rotation = Quaternion.LookRotation(myCollider.transform.position);
         }
-    
-
-        //move the projectile to enemyPos
-        /*if (eb.projectilePrefab != null)
-        {
-            eb.projectilePos = Vector3.MoveTowards(eb.turret.transform.position, eb.enemy.transform.position, 3);
-        }
-
-        ProjectileAttack();*/
-    }
-
-    void ProjectileAttack()
-    {
-
-        float dist = Vector3.Distance(eb.projectilePos, eb.enemyPos);
-
-        if (dist < 5)
-        {
-            Debug.Log("Projectile collided with enemy...");
-
-            //damage
-            if (eb.enemyHP > 0)
-            {
-                eb.enemyHP -= 10;
-                Debug.Log("Enemy damaged... " + eb.enemyHP);
-            }
-
-            //death
-            else
-            {
-                Object.Destroy(eb.enemy);
-                Debug.Log("Enemy dead " + eb.enemyHP);
-                eb.coinCount.text += 20;
-                eb.enemySO.isDead = true;
-            }
-
-            Object.Destroy(eb.projectilePrefab);
-        }
-    }
-
-    void TurretAttack()
-    {
-        eb.enemyPos = eb.enemy.transform.position;
-        eb.turretPos = eb.turret.transform.position;
-
-        Instantiate(eb.projectilePrefab, eb.turretPos, Quaternion.identity);
-        eb.projectileRB = GetComponent<Rigidbody>();
-        eb.projectileRB.AddForce(new Vector3(Random.Range(eb.projectileSpeedLow, eb.projectileSpeedHigh), Random.Range(eb.projectileSpeedLow, eb.projectileSpeedHigh)), ForceMode.Impulse);
-        eb.projectilePrefab.transform.position = eb.enemyPos;
-        Debug.Log("Turret attacking...");
     }
 }
